f5a4363931e437d0d57be9b55ff7782bc847bcd4
Istiaque Mahbub Isti <you@example.comistiaquemahbub1@gmail.com>
2024-09-23 19:14:25 +0600

    Noakhali and Feni donate button EventListener added

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ document.getElementById('aid-input').addEventListener('input',function(event){
         document.getElementById('aid-donate-btn').removeAttribute('disabled')
     }
 })
+let noakhaliTotal =0;
+let feniTotal =0;
+let aidTotal =0;
 
 document.getElementById('noakhali-donate-btn').addEventListener('click',function(){
     const balance = textValueNumber('current-balance')
@@ -26,13 +29,41 @@ document.getElementById('noakhali-donate-btn').addEventListener('click',function
     if(inputValue<0){
        return alert('Invalid Input')
     }
+    noakhaliTotal = noakhaliTotal + inputValue
     document.getElementById('current-balance').innerText = balance - inputValue
-    document.getElementById('noakhali-amount').innerText = inputValue
+    document.getElementById('noakhali-amount').innerText = noakhaliTotal
 
     const historyTab = document.getElementById('history')
     const newDataElement = document.createElement('div')
-    newDataElement.innerHTML = `<div  >
-                <h1 class="font-bold text-lg">${inputValue} is donated for ${headText}</h1>
+    newDataElement.innerHTML = `<div class="px-10" >
+                <h1 class="font-bold text-lg">${inputValue}TK. is donated for ${headText}</h1>
+                <p class="text-xs">Date : ${new Date().toLocaleDateString()} <br>
+                Time : ${new Date().toLocaleTimeString()}
+                </p>
+            </div>`
+            document.getElementById('history').insertBefore(newDataElement,historyTab.firstChild)
+})
+
+document.getElementById('feni-donate-btn').addEventListener('click',function(){
+    const balance = textValueNumber('current-balance')
+    const addAmount = textValueNumber('feni-amount')
+    const inputValue = inputValueNumber('feni-input')
+    const headText = document.getElementById('feni-text').innerText
+    if(balance<inputValue)
+    {
+       return alert('Not Enough Balance');
+    }
+    if(inputValue<0){
+       return alert('Invalid Input')
+    }
+    feniTotal = feniTotal + inputValue
+    document.getElementById('current-balance').innerText = balance - inputValue
+    document.getElementById('feni-amount').innerText = feniTotal
+
+    const historyTab = document.getElementById('history')
+    const newDataElement = document.createElement('div')
+    newDataElement.innerHTML = `<div  class="px-10">
+                <h1 class="font-bold text-lg">${inputValue}TK. is donated for ${headText}</h1>
                 <p class="text-xs">Date : ${new Date().toLocaleDateString()} <br>
                 Time : ${new Date().toLocaleTimeString()}
                 </p>
