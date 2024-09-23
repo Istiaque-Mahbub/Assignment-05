7e22bc43bde7eb2fa60dc1336989c90223d45c5d
Istiaque Mahbub Isti <you@example.comistiaquemahbub1@gmail.com>
2024-09-23 18:59:34 +0600

    Noakhali section functionality added

--- js/script.js
+++ js/script.js
@@ -13,3 +13,29 @@ document.getElementById('aid-input').addEventListener('input',function(event){
         document.getElementById('aid-donate-btn').removeAttribute('disabled')
     }
 })
+
+document.getElementById('noakhali-donate-btn').addEventListener('click',function(){
+    const balance = textValueNumber('current-balance')
+    const addAmount = textValueNumber('noakhali-amount')
+    const inputValue = inputValueNumber('noakhali-donate-input')
+    const headText = document.getElementById('noakhali-text').innerText
+    if(balance<inputValue)
+    {
+       return alert('Not Enough Balance');
+    }
+    if(inputValue<0){
+       return alert('Invalid Input')
+    }
+    document.getElementById('current-balance').innerText = balance - inputValue
+    document.getElementById('noakhali-amount').innerText = inputValue
+
+    const historyTab = document.getElementById('history')
+    const newDataElement = document.createElement('div')
+    newDataElement.innerHTML = `<div  >
+                <h1 class="font-bold text-lg">${inputValue} is donated for ${headText}</h1>
+                <p class="text-xs">Date : ${new Date().toLocaleDateString()} <br>
+                Time : ${new Date().toLocaleTimeString()}
+                </p>
+            </div>`
+            document.getElementById('history').insertBefore(newDataElement,historyTab.firstChild)
+})
--- js/shared.js
+++ js/shared.js
@@ -2,3 +2,7 @@ function inputValueNumber(id){
     const inputNumber = Number(document.getElementById(id).value)
     return inputNumber;
 }
+function textValueNumber(id){
+    const textNumber = Number(document.getElementById(id).innerText)
+    return textNumber;
+}
